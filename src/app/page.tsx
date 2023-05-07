ba6e048fedcef45fe6a291af238489631429df39
erico cavalcanti <ecocaval@hotmail.com>
2023-05-07 17:10:15 -0300

    refactor: sign-in and sign-up folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex items-center justify-around">
-      <Link href="user/sign-in">
+      <Link href="/sign-in">
         <button>Sign in</button>
       </Link>
-      <Link href="user/sign-up">
+      <Link href="/sign-up">
         <button>Sign up</button>
       </Link>
     </main>
