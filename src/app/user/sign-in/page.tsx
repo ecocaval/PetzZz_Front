a8475c7922508b9ac70848ef2b8260b737f35db5
erico cavalcanti <ecocaval@hotmail.com>
2023-05-07 16:07:05 -0300

    feat: basic folder structure template

---
+++ src/app/user/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function SignIn() {
+  return (
+    <main className="">
+      <Link href="/">
+        <button>Home</button>
+      </Link>
+    </main>
+  )
+}
